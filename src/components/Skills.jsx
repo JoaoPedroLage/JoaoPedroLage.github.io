857ff4eb3fe9683a842ba383f16e6335aaed752d
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-02-13 23:20:49 -0300

    feat: add open link in a new tab

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -16,12 +16,14 @@ export default function Skills() {
             <img 
             src="https://img.shields.io/badge/C%2B%2B-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white"
             alt="C++ Card with link to page"
+            target="_blank"
             />
           </a>
           <a href="https://docs.python.org/3/">
             <img 
               src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white"
               alt="Python Card with link to page"
+              target="_blank"
             />
           </a>
         </div>
@@ -30,30 +32,35 @@ export default function Skills() {
             <img
               src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"
               alt="JavaScript Card with link to page"
+              target="_blank"
             />
           </a>
           <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">
             <img
               src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
               alt="HRML5 Card with link to page"
+              target="_blank"
             />
           </a>
           <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
             <img
               src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
               alt="CSS3 Card with link to page"
+              target="_blank"
             />
           </a>
           <a href="https://reactjs.org/">
             <img
               src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
               alt="React Card with link to page"
+              target="_blank"
             />
           </a>
           <a href="https://redux.js.org/">
             <img
               src="https://img.shields.io/badge/Redux-593D88?style=for-the-badge&logo=redux&logoColor=white"
               alt="Redux Card with link to page"
+              target="_blank"
             />
           </a>
         </div>
@@ -62,12 +69,14 @@ export default function Skills() {
             <img 
               src="https://img.shields.io/badge/MySQL-005C84?style=for-the-badge&logo=mysql&logoColor=white"
               alt="MySQL Card with link to page"
+              target="_blank"
             />
           </a>
           <a href="https://docs.docker.com/">
             <img 
               src="https://img.shields.io/badge/Docker-2CA5E0?style=for-the-badge&logo=docker&logoColor=white"
               alt="Docker Card with link to page"
+              target="_blank"
             />
           </a>
         </div>
